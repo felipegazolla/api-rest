110e8bc2de707128a2e405ac2414458a72583864
felipe <enggazolla@gmail.com>
2025-02-27 10:10:00 -0300

    variaveis de ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { knex as setupKnex, type Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL must be set')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
